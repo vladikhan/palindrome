964687dfbcb5614e03200340f623d86edd02fd84
Vladi Khan <84037014+vladikhan@users.noreply.github.com>
2023-10-11 10:17:20 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+function isPalindrome(numberAsString) {
+  let characters = numberAsString.split('')
+  let isPalindrome = true
+
+  for (let index = 0; index < characters.length; index++) {
+    const element = characters[index]
+    if (element !== characters[characters.length - 1 - index]) {
+      isPalindrome = false
+    }
+  }
+  return isPalindrome
+}
+console.log(isPalindrome('10001'))
+console.log(isPalindrome('111'))
